60c004b057d25f76e3100355f28651a5d6054d07
Sina Farhadi <ahmadibishop@gmail.com>
2020-05-08 01:02:59 +0430

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,13 @@
 import {options} from './src';
 
 export const Pleex = {
-    collection: function (collection: string /* Collection name */) { 
+    collection: function (collection: string) { 
         if (collection) {
             return options;
         }
         else {
             /* When collection name not included */
-            throw new Error('Collection name is an required parameter at Pleex.collection(...)');
+            throw new Error('Collection name is an required');
         };
     },
 };
