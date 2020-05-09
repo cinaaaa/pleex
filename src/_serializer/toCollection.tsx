c12d77af932ca7f2bf0fbb3354a620d2ce993f02
Sina Farhadi <ahmadibishop@gmail.com>
2020-05-09 21:21:32 +0430

    Update toCollection.tsx

--- src/_serializer/toCollection.tsx
+++ src/_serializer/toCollection.tsx
@@ -8,12 +8,10 @@ export const toCollection = (collection: string, input: object, cb: (arg0: any) 
     
     getCollection(collection, async (data) => {
         try {
-            
             // Now we have collection
             // Write new items to it
             await AsyncStorage.setItem(collection, JSON.stringify([...data, input]));
             cb(true);
-
         } catch (error) {
             // Error writing data
             throw new Error(`Unknown error in write to ${collection} collection`);
