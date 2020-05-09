6a5d022f473545bf04580db82a4a1e7984551fea
Sina Farhadi <ahmadibishop@gmail.com>
2020-05-09 21:21:58 +0430

    Update getCollection.tsx

--- src/_serializer/getCollection.tsx
+++ src/_serializer/getCollection.tsx
@@ -6,16 +6,13 @@ import {AsyncStorage} from 'react-native';
 export const getCollection = async (collection: string, cb: (arg0: any) => void) => {
 
     try {
-
         let data = await AsyncStorage.getItem(collection);
 
         if (data != null) {/* Data exist, return parsed of data */ cb(JSON.parse(data));}
-
         else {
             await AsyncStorage.setItem(collection, "[]");
             cb([]);
         };
-
     } catch (error) {
        // Error retrieving data
         throw new Error(`error accured: ${error}`);
