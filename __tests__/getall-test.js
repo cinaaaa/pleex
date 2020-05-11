41ddcb7e20a8f70a82e2005d4a36d5d82bde32e7
Sina <ahmadibishop@gmail.com>
2020-05-11 23:17:20 +0430

    some another tests adds

--- __tests__/getall-test.js
+++ __tests__/getall-test.js
@@ -1,20 +1,18 @@
 import {Pleex} from '../index';
 jest.useFakeTimers();
 
-it('schema test', () => {
-
-    // varible to put all data in
-    let final;
+it('Test Get All', done => {
 
     // Create collection first
     let my_collection = Pleex.collection('test');
-
     my_collection.getAll(
-        (data) => console.log(data),
-        (error) => final = error
-    )
+        (data) => {
+            expect(data).toEqual([]);
+            done();
+        },
+        (error) => {
+            console.error(error);
+        }
+    );
 
-    console.log(final);
-
-    expect(typeof final).toEqual('object');
 });
