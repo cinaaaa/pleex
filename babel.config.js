11a874a5574a85368223c1a7ffa7c749695e381e
Sina <ahmadibishop@gmail.com>
2020-05-12 22:59:16 +0430

    del unused

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,5 @@
 module.exports = {
   "presets": [
     "module:metro-react-native-babel-preset",
-  ],
-  "plugins": [
-    ["module-resolver", {
-      "root": ["./src"],
-      "extensions": [".js"]
-    }],
-    "@babel/plugin-transform-runtime",
-  ],
-  "env": {
-    "production": {
-      "plugins": ["transform-remove-console"]
-    },
-  }
+  ]
 }
